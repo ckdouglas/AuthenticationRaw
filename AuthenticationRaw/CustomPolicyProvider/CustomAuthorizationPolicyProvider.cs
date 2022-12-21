235fb12a56f5659b212aeb65ddfb2c8e2dc77778
ckdouglas <douglas.ck.work@gmail.com>
2022-12-21 17:23:29 +0300

    sync

---
+++ AuthenticationRaw/CustomPolicyProvider/CustomAuthorizationPolicyProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace Basics.CustomPolicyProvider
+{
+    public class CustomAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+    {
+
+        public static class DynamicPolicies
+        {
+            public static IEnumerable<string> Get()
+            {
+                yield return SecurityLevel;
+                yield return Rank;
+            }
+            public const string SecurityLevel = "SecurityLevel";
+            public const string Rank = "Rank";
+        }
+
+        public CustomAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options) : base(options)
+        {
+        }
+
+        public override Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+        {
+            return base.GetPolicyAsync(policyName);
+        }
+    }
+}
+
